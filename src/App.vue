f7befc4d6fda80137ccbdb8531a2e098b4940b53
moisesjsanchez <moijsanchez@gmail.com>
2020-01-19 01:16:47 -0800

    initial commit. Created shell frontend

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ProgGenerator msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import ProgGenerator from './components/ProgGenerator.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ProgGenerator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ProgGenerator.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="app">
+    <h1>Prog Generator</h1>
+    <p>To create some guide lines on your piece click the new idea button</p>
+    <div class="container">
+      <div class="btn">
+        Scale
+      </div>
+      <div class="btn">
+        Technique
+      </div>
+      <div class="btn">
+        Time Signature
+      </div>
+    </div>
+    <div class="center-container">
+      <div class="random-btn">
+        New Idea
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<!-- Styling for application -->
+<style scoped>
+
+.app{
+  margin: 0 auto;
+  width: 80%;
+}
+
+.container{
+  display: grid;
+  grid-template-columns: repeat(3,1fr);
+  grid-auto-rows: minmax(50px,auto);
+  grid-gap: 10px;
+  grid-column: 1/4;
+}
+
+.btn{
+  display: flex;
+  background-color: #eee;
+  border: 1px solid #333;
+  align-items: center;
+  justify-content: center;
+}
+
+.random-btn{
+  display: flex;
+  background-color: #eee;
+  border: 1px solid #333;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
