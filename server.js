7a5f01d783672b32b6cf2bcd02587990410ec63f
moisesjsanchez <moijsanchez@gmail.com>
2020-01-20 21:26:31 -0800

    fixed file naming error

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const path = require('path')
 const app = express()
 
 // serve from from our public directory
-app.use('/', serveStatic(path.join(__dirname, '/public')))
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 const port = process.env.PORT || 8080
 app.listen(port)
+console.log(`app is listening on port: ${port}`)
